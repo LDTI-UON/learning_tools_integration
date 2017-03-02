fba8b2b29da1b6b4fb328d790949b7e0b3e62d3d
Paul Sijpkes <paul.sijpkes@gmail.com>
2017-03-02 13:42:59 +1100

    fixing UI bug

--- addon.setup.php
+++ addon.setup.php
@@ -5,7 +5,7 @@ return array(
       'author_url'  => 'https://bold.newcastle.edu.au',
       'name'        => 'UoN Learning Tools Integration',
       'description' => 'University of Newcastle LTI tool launch and member management support',
-      'version'     => '3.3.0',#build version#
+      'version'     => '3.3.6',#build version#
       'namespace'   => 'Learning_tools_integration',
       'settings_exist' => TRUE,
 );
