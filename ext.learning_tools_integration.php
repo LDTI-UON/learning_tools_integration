27f8e229cb4078fd898a535f04513bfbe21e914e
Paul Sijpkes <paul.sijpkes@gmail.com>
2017-03-24 14:52:06 +1100

    fixed &*^&^ up

--- ext.learning_tools_integration.php
+++ ext.learning_tools_integration.php
@@ -483,7 +483,6 @@ class Learning_tools_integration_ext {
 			! ( strpos($roles,"A") === FALSE );
 
 		$this->isInstructor = $context->isInstructor() || $bb_instructor;
-		$this->isInstructor = true;
 
 		if (! $this->isInstructor ) {
 			$this -> lis_result_sourcedid = isset($_REQUEST["lis_result_sourcedid"]) ? ee('Security/XSS')->clean($_REQUEST["lis_result_sourcedid"]) : 'not set';
