f6c38a16c730004a379ed568af94b75aa671c851
Paul Sijpkes <paul.sijpkes@newcastle.edu.au>
2018-02-19 11:34:38 +1100

    bootbox fix

--- views/modal.php
+++ views/modal.php
@@ -4,7 +4,7 @@
 </div>
 
 <script>
-var lti_M = lti_M || { message: $('#modal').html() };
+var lti_M = lti_M || { message: document.getElementById('modal').innerHTML };
 
 <?php if(isset($callback_f)): ?>
   var callback_f = <?= $callback_f ?>;
@@ -20,13 +20,24 @@ var lti_M = lti_M || { message: $('#modal').html() };
   lti_M.size = '<?= $size ?>' || null;
 <?php endif; ?>
 
-$(document).ready(function() {
+document.addEventListener("DOMContentLoaded", function(event) {
+    lti_M.show = true;
+    lti_M.backdrop = "static";
     bootbox.alert(lti_M);
+
+    let nodes = document.getElementsByClassName('modal-body');
+
+    // hack to fix bootbox message
+    for(let i = 0; nodes.item(i); i++) {
+        nodes.item(i).style.backgroundColor = "#eee";
+        nodes.item(i).innerHTML = lti_M.message;
+    }
 });
+
 
 <?php if (isset($onHidden)): ?> //jshint ignore:line
   $("#modal").on('hidden.bs.modal', function () {//jshint ignore:line
-    <?php if(isset($onHidden)) echo $onHidden; ?> //jshint ignore:line
+      <?php if(isset($onHidden)) echo $onHidden; ?> //jshint ignore:line
   });//jshint ignore:line
 <?php endif; ?> //jshint ignore:line
 
