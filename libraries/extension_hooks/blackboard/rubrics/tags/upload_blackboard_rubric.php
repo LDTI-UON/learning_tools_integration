66fcd1c93d3f5bdc33db7cebfcb4acd3e42bdd47
Paul Sijpkes <ps158@cdb208-dyhtdd.newcastle.edu.au>
2018-02-19 12:58:29 +1100

    fixed img path

--- libraries/extension_hooks/blackboard/rubrics/tags/upload_blackboard_rubric.php
+++ libraries/extension_hooks/blackboard/rubrics/tags/upload_blackboard_rubric.php
@@ -189,7 +189,7 @@ $hook_method = function() {
 //  $form .= form_label('Attach this rubric:  ', 'attach', array('for' => 'attach'));
 
   $form .= form_button('Attach Rubric to Assessment', 'attach', "id='attach' class='$this->button_class form-control'");
-  $form .= "<img id='rub_loader' src='".URL_THEMES."/learning_tools_integration/img/loader.gif' style='display:none'/><span id='loader_msg'></span>";
+  $form .= "<img id='rub_loader' src='".URL_THIRD_THEMES."learning_tools_integration/img/loader.gif' style='display:none'/><span id='loader_msg'></span>";
   $form .= "</p>";
   $form .= form_close();
 
