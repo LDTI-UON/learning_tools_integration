d8447aa4e9005ba2fb840c566af0563ccc987cc1
Paul Sijpkes <paul.sijpkes@newcastle.edu.au>
2018-03-15 10:42:27 +1100

    cheap

--- config/lti_config.php
+++ config/lti_config.php
@@ -3,7 +3,7 @@
 # @Date:   2017-03-28T16:04:23+11:00
 # @Last modified by:   ps158
 # @Last modified time: 2017-04-21T11:37:20+10:00
-$config['development'] = TRUE;
+$config['development'] = FALSE;
 // cache areas
 $config['lti_cache'] = '/var/www/lti/cache/';
 $config['lti_upload'] = "/var/www/lti/cache/upload/";
